b024a39193a478d2c40887c913dd738b0ae64661
evanca <33544598+evanca@users.noreply.github.com>
2018-09-06 15:14:35 +0300

    Implement column widget & add a logo image

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,20 @@ class MyApp extends StatelessWidget {
             image: new DecorationImage(
                 image: new AssetImage('images/bg.png'), fit: BoxFit.cover),
           ),
-          child: Center(
-            child: Text('Hello World'),
+          // Column widget that displays its children in a vertical array
+          child: Column(
+            children: [
+              Image.asset('images/logo.png',
+                  width: 225.0, height: 225.0, fit: BoxFit.contain),
+              Center(
+                child: Text(
+                    '2814  Morningview Lane\nRedan, GA 30074\n641-998-7204'),
+              ),
+              Center(
+                child: Text(
+                    'There is a powerful driving force inside every human being that, once unleashed, can make any vision, dream, or desire a reality.'),
+              ),
+            ],
           ),
         ),
       ),
